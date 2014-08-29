6dc7aac3b8207429d47b5c2d421b61daa5cfb44d
Vishnu.R <cethal.mary@cabotsolutions.com>
2014-08-29 17:20:52 +0530

    nickname changed to new user

--- examples/chat/app.js
+++ examples/chat/app.js
@@ -66,41 +66,70 @@ mongoose.connect('mongodb://localhost/chat',function(err){
 });
 
 var io = sio.listen(app)
-    , nicknames = {};
-
-io.sockets.on('connection', function (socket) {
-    socket.on('room', function(room) {
-        console.log('!!!!!!!!!!!!!!',room);
-        socket.join(room);
-        socket.broadcast.emit('announcement', room + ' created');
-    });
+    , users = {};
 
 
-    socket.on('user message', function (message) {
-        socket.broadcast.emit('user message',message);
-    });
+io.sockets.on('connection', function(socket){
+    socket.on('new user', function(data, callback){
 
-    socket.on('nickname', function (nick, fn) {
-        console.log('function ',fn);
-        if (nicknames[nick])
-        {
-            console.log('true case',nicknames[nick]);
-            fn(true);
-        } else
-        {
-            console.log('false case',nicknames[nick]);
-
-            fn(false);
-            nicknames[nick] = socket.nickname = nick;
-            socket.broadcast.emit('announcement', nick + ' connected');
-            io.sockets.emit('nicknames', nicknames);
+        console.log('data format',data);
+        if (data in users){
+            callback(false);
+        } else{
+            callback(true);
+            socket.nickname = data;
+            users[socket.nickname] = socket;
+            updateNicknames();
         }
     });
 
-    socket.on('disconnect', function () {
-        if (!socket.nickname) return;
-        delete nicknames[socket.nickname];
-        socket.broadcast.emit('announcement', socket.nickname + ' disconnected');
-        socket.broadcast.emit('nicknames', nicknames);
+    function updateNicknames(){
+        io.sockets.emit('usernames', Object.keys(users));
+    }
+
+
+    function createGroup()
+    {
+
+        socket.on('createRoom', function (data, callback) {
+            socket.join(data);
+
+        })
+    }
+
+    function listRooms()
+    {
+        socket.on('listRooms', function(data,callback){
+            console.log('listing all rooms',callback.manager);
+        })
+    }
+    socket.on('send message', function(data, callback){
+        var msg = data.trim();
+        console.log('after trimming message is: ' + msg);
+        if(msg.substr(0,3) === '/w '){
+            msg = msg.substr(3);
+            var ind = msg.indexOf(' ');
+            if(ind !== -1){
+                var name = msg.substring(0, ind);
+                var msg = msg.substring(ind + 1);
+                if(name in users){
+                    users[name].emit('whisper', {msg: msg, nick: socket.nickname});
+                    console.log('message sent is: ' + msg);
+                    console.log('Whisper!');
+                } else{
+                    callback('Error!  Enter a valid user.');
+                }
+            } else{
+                callback('Error!  Please enter a message for your whisper.');
+            }
+        } else{
+            io.sockets.emit('new message', {msg: msg, nick: socket.nickname});
+        }
+    });
+
+    socket.on('disconnect', function(data){
+        if(!socket.nickname) return;
+        delete users[socket.nickname];
+        updateNicknames();
     });
 });
